842d170dbad3efc48a8b20bec2ac83a2a6442c1d
Daniel Martinez <dmartmar@mercadona.es>
2022-06-09 17:27:22 +0200

    feat: Added countries to html

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Pais } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  isBusquedaError: boolean = false;
+  private _paises: Pais[] = [];
+
+  get paises() : Pais[] {
+    return [...this._paises];
+  }
+
+  constructor( private paisService: PaisService ) { }
 
   ngOnInit(): void {
+  }
+
+  buscar() {
+    this.isBusquedaError = false;
+
+    this.paisService.getPais(this.termino).subscribe(
+      (paises: Pais[]) => {
+        this._paises = paises;
+      },
+      (error: HttpErrorResponse) => {
+        this.isBusquedaError = true;
+        this._paises = [];
+      }
+    );
   }
 
 }
---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pais } from '../interfaces/pais.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private _apiUrl = 'https://restcountries.com/v3.1';
+
+  constructor( private ngHttpClient: HttpClient ) { }
+
+  getPais( termino: string ): Observable<Pais[]> {
+
+    const url = `${this._apiUrl}/name/${termino}`;
+    return this.ngHttpClient.get<Pais[]>(url);
+  }
+}
