c4dad8959beaee7e91c4dbf0385c1e4ab47092b3
DaniMartinezMarin <martinez.marin.dani>
2022-06-14 20:30:11 +0200

    feat: Pagina para ver detalle del pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,37 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
 import { Pais } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styleUrls: ['./ver-pais.component.css']
+  styleUrls: ['./ver-pais.component.css'],
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor( 
+  pais!: Pais;
+  Object = Object;
+
+  constructor(
     private ngActivatedRoute: ActivatedRoute,
-    private ngPaisService: PaisService 
-  ) { }
+    private ngPaisService: PaisService
+  ) {}
 
   ngOnInit(): void {
 
-    this.ngActivatedRoute.params
-      .subscribe( 
-        ( {id} ) => { //desestructuracion
-          console.log(id); 
-          this.ngPaisService.getPaisPorId(id)
-            .subscribe(
-              (pais: Pais[]) => {
-                console.log(pais);
-              },
-              (error) => {
-                console.error('Se ha producido un error: ', error);
-              }
-            )
-        }
-      )
+    this.ngActivatedRoute.params.pipe(
+      switchMap((param) => this.ngPaisService.getPaisPorId(param.id)),
+      tap( console.log )
+    )
+    .subscribe(
+      (paises: Pais[]) => {
+        this.pais = paises[0];                
+      }
+    );
+
   }
 
 }
